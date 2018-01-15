1e2ec0f2f72d4f9acd60af4c8d8b3948b18988aa
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-01-15 04:07:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var animal = 'dog'
+animal = 'cat'
 
 function myAnimal() {
   return animal
@@ -12,8 +12,6 @@ function yourAnimal() {
 }
 
 function add2(n) {
-
-
   // Feel free to move things around!
   const two = 2
   return n + two
